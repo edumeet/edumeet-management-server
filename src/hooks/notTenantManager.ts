54e7a40187db6e9602fd3226f7ec87b7449f2b3d
Rémai Gábor <remai.gabor@gmail.com>
2023-11-06 18:36:07 +0100

    fix condition for tenantManager

--- src/hooks/notTenantManager.ts
+++ src/hooks/notTenantManager.ts
@@ -2,6 +2,7 @@ import { HookContext } from '../declarations';
 
 export const notTenantManager = () => (context: HookContext) => {
 	if (!context.params.user) return true;
-
-	return !context.params.user.tenantAdmin || !context.params.user.tenantOwner;
+    
+	// Not tenant admin and not owner 
+	return !context.params.user.tenantAdmin && !context.params.user.tenantOwner;
 };
