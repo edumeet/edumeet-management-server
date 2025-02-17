fe14031c10dea2dc93fb946447719792d7939e74
Rémai Gábor <remaigabor@gmail.com>
2025-02-17 14:09:57 +0100

    fix notInSameTenant type mismatch

--- src/hooks/notSameTenant.ts
+++ src/hooks/notSameTenant.ts
@@ -1,6 +1,6 @@
 import { HookContext } from '../declarations';
 
 export const notInSameTenant = (context: HookContext) => {
-	if (context.data.tenantId !== context.params.user.tenantId)
+	if (parseInt(context.data.tenantId) !== parseInt(context.params.user.tenantId))
 		throw new Error('You are not in the same tenant');
 };
