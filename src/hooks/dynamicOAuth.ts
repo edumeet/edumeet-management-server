d19f53f641c6129d89fcbbda45836334a509e262
N7Remus <remaigabor@gmail.com>
2025-12-15 14:36:18 +0100

    tweak startup hook

--- src/hooks/dynamicOAuth.ts
+++ src/hooks/dynamicOAuth.ts
@@ -101,60 +101,61 @@ export const dynamicOAuthSetup = async (context: HookContext, next: NextFunction
 			// get tenant data
 			const { total, data } = await context.app.service('tenantOAuths').find({ query: { tenantId } });
 
-			if (total === 0) throw new Error('Missing OAuth configuration');
+			if (total !== 0) {
 
-			const {
-				key,
-				secret,
-				authorize_url,
-				access_url,
-				profile_url,
-				redirect_uri = '/oauth/tenant/callback',
-				scope = 'openid profile email',
-				scope_delimiter = ' ',
-				name_parameter
-			} = data[0];
+				const {
+					key,
+					secret,
+					authorize_url,
+					access_url,
+					profile_url,
+					redirect_uri = '/oauth/tenant/callback',
+					scope = 'openid profile email',
+					scope_delimiter = ' ',
+					name_parameter
+				} = data[0];
 
-			if (!key) throw new Error('Missing OAuth key');
-			if (!secret) throw new Error('Missing OAuth secret');
-			if (!authorize_url) throw new Error('Missing OAuth authorize_url');
-			if (!access_url) throw new Error('Missing OAuth access_url');
-			if (!profile_url) throw new Error('Missing OAuth profile_url');
+				if (!key) throw new Error('Missing OAuth key');
+				if (!secret) throw new Error('Missing OAuth secret');
+				if (!authorize_url) throw new Error('Missing OAuth authorize_url');
+				if (!access_url) throw new Error('Missing OAuth access_url');
+				if (!profile_url) throw new Error('Missing OAuth profile_url');
 
-			// get live config
-			const config = context.app.get('authentication');
+				// get live config
+				const config = context.app.get('authentication');
 
-			if (config?.oauth) {
+				if (config?.oauth) {
 				// eslint-disable-next-line @typescript-eslint/no-explicit-any
-				const tmpconf: any = config.oauth || {};
+					const tmpconf: any = config.oauth || {};
 
-				tmpconf[tenantId] = {
+					tmpconf[tenantId] = {
 					// tmpconf['tenant'] = {
-					'oauth': 2,
-					'key': key,
-					'client_id': key,
-					'secret': secret,
-					'scope': scope,
-					'authorize_url': authorize_url,
-					'access_url': access_url,
-					'profile_url': profile_url,
-					'redirect_uri': redirect_uri,
-					'scope_delimiter': scope_delimiter,
-					'name_parameter': name_parameter,
-					'token_endpoint_auth_method': 'client_secret_basic'
-				};
-				config.oauth = tmpconf;
-				// update config 
-				context.app.set('authentication', config);
-				// delete oauth 
-				context.app.unuse('oauth/:provider');
-				// updateConfiguration
-				context.app.configure(oauth());
-				// re-add hooks
-				context.app.service('oauth/:provider').hooks(
-					{
-						before: { find: [ dynamicOAuth ] },
-					});
+						'oauth': 2,
+						'key': key,
+						'client_id': key,
+						'secret': secret,
+						'scope': scope,
+						'authorize_url': authorize_url,
+						'access_url': access_url,
+						'profile_url': profile_url,
+						'redirect_uri': redirect_uri,
+						'scope_delimiter': scope_delimiter,
+						'name_parameter': name_parameter,
+						'token_endpoint_auth_method': 'client_secret_basic'
+					};
+					config.oauth = tmpconf;
+					// update config 
+					context.app.set('authentication', config);
+					// delete oauth 
+					context.app.unuse('oauth/:provider');
+					// updateConfiguration
+					context.app.configure(oauth());
+					// re-add hooks
+					context.app.service('oauth/:provider').hooks(
+						{
+							before: { find: [ dynamicOAuth ] },
+						});
+				}
 			}
 		}
 	}
