535d29ad6e6b0c561af03503aa224607e7f9e8b9
Piotr Pawałowski <piotr.pawalowski@medvc.eu>
2026-01-08 10:19:20 +0100

    Fix login 10

--- src/hooks/requireNonEmptyName.ts
+++ src/hooks/requireNonEmptyName.ts
@@ -3,13 +3,13 @@ import { BadRequest } from '@feathersjs/errors';
 
 export const requireNonEmptyName = async (context: HookContext): Promise<HookContext> => {
 	if (context.data && Object.prototype.hasOwnProperty.call(context.data, 'name')) {
-		const n = String((context.data as any).name ?? '').trim();
+		const n = String(context.data.name ?? '').trim();
 
 		if (n.length === 0) {
 			throw new BadRequest('Name is required');
 		}
 
-		(context.data as any).name = n;
+		context.data.name = n;
 	}
 
 	return context;
